0552ace3a668a6d522819dea7b5c707eac1c9ec5
mike.lee <mybios@qq.com>
2017-12-23 04:13:07 +0800

    发布

---
+++ dist/lib/logger.d.ts
@@ -0,0 +1,16 @@
+declare function getLogger(categoryName: any): {};
+/**
+ * Configure the logger.
+ * Configure file just like log4js.json. And support ${scope:arg-name} format property setting.
+ * It can replace the placeholder in runtime.
+ * scope can be:
+ *     env: environment variables, such as: env:PATH
+ *     args: command line arguments, such as: args:1
+ *     opts: key/value from opts argument of configure function
+ *
+ * @param  {String|Object} config configure file name or configure object
+ * @param  {Object} opts   options
+ * @return {Void}
+ */
+declare function configure(config: any, opts: any): void;
+export { getLogger, configure };
